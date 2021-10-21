5feb2d2fd79c8d280c0c708d41077065e2227f3e
RonaldsonBellande <ronaldsonbellande@gmail.com>
2021-10-21 17:00:02 -0400

    recal

--- Algorithm_analysis.py
+++ Algorithm_analysis.py
@@ -18,3 +18,21 @@ if __name__ == "__main__":
     king = sarsa_algorithm(episode=10000, gamma=1, alpha=0.5, epsilon=0.1, max_time_step=10000, action_type="king", wind_type="not_regular")
     king_zero_time_step_not_normal = king.sarsa()
     king.plot_episode_time_step()
+
+
+    number_episode = 10000
+    regular = q_learning_algorithm(episode=number_episode, gamma=1, alpha=1, epsilon=0.1, max_time_step=10000, action_type="regular", wind_type="regular")
+    regular.q_learning()
+    regular.plot_episode_time_step(type_graph = "reward", type_graph_name="Q_Learning reward | alpha 1")
+    regular.plot_episode_time_step(type_graph = "action", type_graph_name="Q Learning action | alpha 1")
+    regular.double_q_learning()
+    regular.plot_episode_time_step(type_graph = "reward", type_graph_name="Double Q_Learning reward | alpha 1")
+    regular.plot_episode_time_step(type_graph = "action", type_graph_name="Double Q Learning action | alpha 1")
+ 
+    regular = q_learning_algorithm(episode=number_episode, gamma=1, alpha=0.8, epsilon=0.1, max_time_step=10000, action_type="regular", wind_type="regular")
+    regular.q_learning()
+    regular.plot_episode_time_step(type_graph = "reward", type_graph_name="Q_Learning reward | alpha 0.8")
+    regular.plot_episode_time_step(type_graph = "action", type_graph_name="Q Learning action | alpha 0.8")
+    regular.double_q_learning()
+    regular.plot_episode_time_step(type_graph = "reward", type_graph_name="Double Q_Learning reward | alpha 0.8")
+    regular.plot_episode_time_step(type_graph = "action", type_graph_name="Double Q Learning action | alpha 0.8")
