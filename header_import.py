af0c938728a8f4fa0c58e7768ea35877d98326b2
RonaldsonBellande <ronaldsonbellande@gmail.com>
2021-10-21 12:51:27 -0400

    ok

--- header_import.py
+++ header_import.py
@@ -3,6 +3,6 @@ import matplotlib.pyplot as plt
 from collections import defaultdict
 import matplotlib.patches as patches
 import random
-from Windy_World import *
+from Windy_World_Grid_Enviroment import *
 from Algorithm_analysis import *
 from plot_graphs import *
--- plot_graphs.py
+++ plot_graphs.py
@@ -1,2 +1,28 @@
 from header_import import *
 
+class plot_graphs(object):
+    def __init__(self):
+        self.path = "graphs_charts/"
+
+
+
+    def plot_episode_time_step(self, type_graph = "reward", type_graph_name = "default"):
+
+        fig = plt.figure()
+        axis = fig.add_subplot(111)
+        if type_graph == "reward":
+            axis.plot(self.reward_average, color='blue')
+            plt.axhline(y=0.2, color='red', linestyle='-')
+            axis.set_title(type_graph_name+ " Reward vs Time Step")
+            axis.set_xlabel("Time Steps")
+            axis.set_ylabel("Reward per Step")
+        elif type_graph == "action":
+            axis.plot(self.max_action, color='blue')
+            axis.set_title(type_graph_name + " Maximal Action vs Time Step")
+            plt.axhline(y=0.36, color='red', linestyle='-')
+            axis.set_xlabel("Time Steps")
+            axis.set_ylabel("Maximal Action Value")
+        plt.savefig((str(self.path) + type_graph_name + "_" + type_graph + "_policy_graphs.png"), dpi =500)
+
+
+
